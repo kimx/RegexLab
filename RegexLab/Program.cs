57c7fe31912cbbe1055ccd0fc2d38a0a9b17a207
kimx <kimxinfo@gmail.com>
2016-07-22 16:55:02 +0800

    Add project files.

---
+++ RegexLab/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegexLab
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Begin");
+            TestReplaceSymbol();
+            Console.WriteLine("End");
+            Console.ReadLine();
+        }
+
+        private static void TestReplaceSymbol()
+        {
+            string value = "#Column#_#Test#";
+
+            Console.WriteLine("TestReplaceSymbol-Begin");
+            string fixdeValue = value;
+            if (fixdeValue.StartsWith("#"))
+                fixdeValue = "@@@@@" + fixdeValue;
+            string[] values = fixdeValue.Split(new string[] { "#" }, StringSplitOptions.RemoveEmptyEntries);
+            List<string> list = new List<string>();
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (i % 2 == 0)
+                    continue;
+
+                var t = string.Format("#{0}#", values[i]);
+                if (list.IndexOf(t) > -1)
+                    continue;
+                list.Add(t);
+            }
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("TestReplaceSymbol-End");
+        }
+
+
+    }
+}
